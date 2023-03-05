cbcafb130811be4dc327a99b5ce1456e27483798
Hiro0803-HK <kd1339439@st.kobedenshi.ac.jp>
2023-03-05 14:05:09 +0900

    Django ENV

---
+++ db/init/02_insert_data.sql
@@ -0,0 +1,11 @@
+INSERT INTO book VALUES(DEFAULT, 'プログラミング言語C', current_timestamp);
+
+INSERT INTO book VALUES(DEFAULT, '優しいコンピューター科学', current_timestamp);
+
+INSERT INTO book VALUES(DEFAULT, 'TeXブック コンピュータによる組版システム', current_timestamp);
+
+INSERT INTO book VALUES(DEFAULT, 'ゲーテル、エッシャー、バッハ＿あるいは不思議の環', current_timestamp);
+
+INSERT INTO book VALUES(DEFAULT, 'Texブック コンピュータによる組版システム', current_timestamp);
+
+INSERT INTO book VALUES(DEFAULT, '人月の神話 狼人間を撃つ銀の弾はない', current_timestamp);
